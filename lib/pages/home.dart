5d4bdc5c121c7530a259dde666d9ef787b3bf303
Guoyong Duan <gydlake@gmail.com>
2022-02-22 12:45:41 +0200

    Current temperature of Oulu

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -22,20 +22,102 @@ class _HomeState extends State<Home> {
     wd=ModalRoute.of(context)?.settings.arguments as WeatherData;
     Color? bgColor=wd.is_day==1?Colors.blue[300]:Colors.grey[600];
 
+    List<String> weatherInfo= [
+      'test',
+      'adf',
+      'ttt'
+      // 'FeelsLike: ${wd.feelslike_c}\u1d52C, Wind: ${wd.wind}${wd.wind_dir}',
+      // 'Precipitation: ${wd.precip_mm}, Humidity: ${wd.humidity}',
+      // 'Visibility in KM: ${wd.vis_km}, UV: ${wd.uv}'
+    ];
+
     return SafeArea(
-      child: Scaffold(
-        backgroundColor: bgColor,
-        body: Column(
-          children: [
-            Image.network(
-                'https:${wd.condition_icon}',
-              width: 150.0,
-              height: 150.0,
-              fit: BoxFit.cover,
+        child: Scaffold(
+          backgroundColor: bgColor,
+          body: SingleChildScrollView(
+            physics: ScrollPhysics(),
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Image.network(
+                      'https:${wd.condition_icon}',
+                      width: 150.0,
+                      height: 150.0,
+                      fit: BoxFit.cover,
+                    ),
+                    SizedBox(width: 20.0,),
+                    FlatButton.icon(
+                      onPressed: (){},
+                      icon: Icon(Icons.edit_location),
+                      label: Container(
+                        width: 150, // change width as you need
+                        height: 100, // change height as you need
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            '${wd.location} ${wd.country}',
+                            textAlign: TextAlign.left,
+                            maxLines: 2, // change max line you need
+                            style: TextStyle(
+                                fontSize: 30.0,
+                                letterSpacing: 2.0,
+                                color: Colors.red[600]
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+
+                  ],
+                ),
+
+                SizedBox(height: 20.0,),
+                Text(
+                  '${wd.temp_c}\u1d52C, ${wd.condition_txt} ',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    letterSpacing: 2.0,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(height: 20.0,),
+                Text(
+                  'FeelsLike: ${wd.feelslike_c}\u1d52C, Wind: ${wd.wind}${wd.wind_dir}',
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    letterSpacing: 2.0,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(height: 20.0,),
+                Text(
+                  'Precipitation: ${wd.precip_mm}, Humidity: ${wd.humidity}',
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    letterSpacing: 2.0,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(height: 20.0,),
+                Text(
+                  'Visibility in Km: ${wd.vis_km}, UV: ${wd.uv}',
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    letterSpacing: 2.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+          ),
             ),
-            SizedBox(height: 20.0,),
-            Text(wd.country!),
-          ],
         ),
       ),
     );
--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'dart:convert';
 import 'package:world_weather/services/weather_data.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
@@ -27,7 +28,7 @@ class _LoadingState extends State<Loading> {
   }
 
   void setWeather() async{
-    await wd.getWeather('Helsinki');
+    await wd.getWeather('Oulu');
     print(wd.country);
     Navigator.pushReplacementNamed(context, '/home', arguments: wd);
   }
@@ -36,12 +37,25 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[100],
       body: SafeArea(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+
           children: [
-            Text('Loading'),
+            Text(
+                'Loading',
+              style: TextStyle(
+                  fontSize: 40.0,
+                  letterSpacing: 2.0,
+                  color: Colors.red[600]
+              ),
+            ),
             SizedBox(height: 30.0),
-            
+            SpinKitFadingCircle(
+              color: Colors.blue,
+              size: 100.0,
+            ),
           ],
         ),
       ),
